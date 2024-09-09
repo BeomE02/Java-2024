804189548168603dad934f8daf646ebb4fba4ef2
BeomE02 <80086144+BeomE02@users.noreply.github.com>
2024-09-09 11:22:22 +0900

    0909

---
+++ Hello.java
@@ -0,0 +1,27 @@
+package hello.eclipse;
+
+public class Hello {
+	public static int sum(int n, int m) {
+		return n + m;
+	}
+	
+	public static void main(String[] args) {
+		int i = 20;
+		int s;
+		char a;
+		
+		a = '?';
+		s = sum(i, 10);
+		
+		System.out.println(a);
+		System.out.println("Hello \n");
+		System.out.println(s);
+		
+		//System.out.println(a);
+		/*System.out.println("Hello \n");
+		System.out.println(s);*/
+		
+		//두가지의 주석 처리 방법
+	}
+}
+
