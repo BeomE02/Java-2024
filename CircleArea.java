5f3ff8f5ba77c84dbc0188923a90bcee580beeca
BeomE02 <80086144+BeomE02@users.noreply.github.com>
2024-09-09 11:49:38 +0900

    0909-2

---
+++ CircleArea.java
@@ -0,0 +1,19 @@
+package hello.eclipse;
+
+public class CircleArea {
+	
+	public static void main(String[] args) {
+		final double PI = 3.141592;
+		double radius = 10.0;
+		double circleArea;
+		
+		int i = 6;
+		byte b = (byte)i;
+		// cast 연산자 위의 연산이 실행될 떄만 int를 byte 타입으로 변경합니다.
+		
+		circleArea = PI * radius * radius;
+		
+		System.out.println("원의 면적 = " + circleArea);
+	}
+	
+}
