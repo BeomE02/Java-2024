6096873421ce5aa3f44828974bd67820a9236391
BeomE02 <80086144+BeomE02@users.noreply.github.com>
2024-09-21 22:00:59 +0900

    Add files via upload

---
+++ MyInfoApp.java
@@ -0,0 +1,10 @@
+package java2024;
+
+public class MyInfoApp {
+	public static void main(String[] args) {
+		System.out.println("하재범");
+		System.out.println("23");
+		System.out.println("컴퓨터소프트웨어");
+	}
+
+}
