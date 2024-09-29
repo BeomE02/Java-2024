bfddf7d13761a7a3ff51b2c9a5b29f55cf0509c7
BeomE02 <80086144+BeomE02@users.noreply.github.com>
2024-09-29 22:41:08 +0900

    Create MultiplesOfThree.java

---
+++ report_02/MultiplesOfThree.java
@@ -0,0 +1,29 @@
+package java_2024;
+
+import java.util.Scanner;
+
+public class MultiplesOfThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[18]; // 양의 정수 18개를 저장할 배열
+
+        System.out.print("양의 정수 18개 입력>> ");
+        
+        // 18개의 양의 정수 입력 받기
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        System.out.print("3의 배수는.. ");
+
+        // 3의 배수 출력
+        for (int number : numbers) {
+            if (number % 3 == 0) {
+                System.out.print(number + " ");
+            }
+        }
+
+        System.out.println(); // 줄 바꿈
+        scanner.close(); // 스캐너 닫기
+    }
+}
