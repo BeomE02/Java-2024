1ac1de802b54b0c19b89ef0fee772785c6a4263e
BeomE02 <80086144+BeomE02@users.noreply.github.com>
2024-09-30 12:38:56 +0900

    Add files via upload

---
+++ 0930/CirlceEx.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class CirlceEx {
+   public String name;
+   public int radius;
+   public CirlceEx() {
+   }
+   public double getArea() {
+      return 3.141592 * radius * radius;
+   }
+}
