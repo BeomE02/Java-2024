0ba1385a1594c3b4e5b6dd79a45805200be6185f
BeomE02 <80086144+BeomE02@users.noreply.github.com>
2024-09-30 12:39:14 +0900

    Add files via upload

---
+++ 0930/MainTest.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class MainTest {
+	public void printInfo() {
+		System.out.println();
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		MainTest mt = new MainTest();
+		mt.printInfo();
+	}
+}
